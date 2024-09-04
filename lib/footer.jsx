a91cca9047bd2c2dcfc7c7684b2668f9390b05cd
Todd <todd@thestark.net>
2024-09-04 12:48:30 -0400

    Finished chat feature

--- lib/footer.jsx
+++ lib/footer.jsx
@@ -3,42 +3,40 @@
 import Link from "next/link"
 import axios from "axios"
 import { useRouter } from "next/navigation"
+import Chat from "./chat"
 
 export default function Footer(props) {
-  function AdminFooterOrNot() {
-    if (props.isAdmin) {
-      return (
-        <p className="logout-text">
-          <a href="" onClick={handleClick}>
-            Logout
-          </a>
-        </p>
-      )
-    } else {
-      return <></>
+    function AdminFooterOrNot() {
+        if (props.isAdmin) {
+            return (<>
+            <Chat />
+            <p className="logout-text">
+                <a href="" onClick={handleClick}>Logout</a>
+            </p>     
+            </>)
+        } else {
+            return <></>
+        }
     }
-  }
 
-  const router = useRouter()
+    const router = useRouter()
 
-  async function handleClick(e) {
-    e.preventDefault()
-    await axios.get("/logout-action")
-    router.replace("/login")
-    router.refresh()
-  }
+    async function handleClick(e) {
+        e.preventDefault()
+        await axios.get("/logout-action")
+        router.replace("/login")
+        router.refresh()
+    }
 
-  return (
-    <>
-      <footer className="site-footer">
+    return (<>
+    <footer className="site-footer">
         {AdminFooterOrNot()}
         <p className="footer-nav">
-          <Link href="/">Home</Link>
-          <Link href="/our-history">Our History</Link>
-          <Link href="/our-vision">Our Vision</Link>
+            <Link href="/">Home</Link>
+            <Link href="/our-history">Our History</Link>
+            <Link href="/our-vision">Our Vision</Link>
         </p>
         <p>&copy; 2023 Fake Adoption Center. All rights reserved.</p>
-      </footer>
-    </>
-  )
+    </footer>
+    </>)
 }
